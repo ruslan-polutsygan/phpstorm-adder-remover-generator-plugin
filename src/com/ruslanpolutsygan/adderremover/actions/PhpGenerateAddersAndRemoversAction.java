2982fc09aa62a3b58e6b4d6b526e0fd037e9fefc
ruslan-polutsygan <ruslan.polutsygan@gmail.com>
2016-01-07 18:53:46 +0200

    display "generate .." actions menu items only in php class context

--- src/com/ruslanpolutsygan/adderremover/actions/PhpGenerateAddersAndRemoversAction.java
+++ src/com/ruslanpolutsygan/adderremover/actions/PhpGenerateAddersAndRemoversAction.java
@@ -2,6 +2,9 @@ package com.ruslanpolutsygan.adderremover.actions;
 
 import com.intellij.codeInsight.CodeInsightActionHandler;
 import com.intellij.codeInsight.actions.CodeInsightAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
 import com.ruslanpolutsygan.adderremover.handler.ActionHandler;
 import com.ruslanpolutsygan.adderremover.handler.checkers.AdderChecker;
 import com.ruslanpolutsygan.adderremover.handler.checkers.ChainedChecker;
@@ -15,10 +18,9 @@ import org.jetbrains.annotations.NotNull;
 
 
 public class PhpGenerateAddersAndRemoversAction extends CodeInsightAction {
+    private final ActionHandler handler;
 
-    @NotNull
-    @Override
-    protected CodeInsightActionHandler getHandler() {
+    public PhpGenerateAddersAndRemoversAction() {
         MethodGenerator generator = new ChainedGenerator(new MethodGenerator[] {
                 new AdderGenerator(),
                 new RemoverGenerator()
@@ -28,6 +30,17 @@ public class PhpGenerateAddersAndRemoversAction extends CodeInsightAction {
                 new RemoverChecker()
         });
 
-        return new ActionHandler(generator, checker);
+        this.handler = new ActionHandler(generator, checker);
+    }
+
+    @NotNull
+    @Override
+    protected CodeInsightActionHandler getHandler() {
+        return this.handler;
+    }
+
+    @Override
+    protected boolean isValidForFile(@NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
+        return this.handler.isValidFor(editor, file);
     }
 }
